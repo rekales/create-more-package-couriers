572d5e951db954dff121f03fb4001021e7594879
Krei <criketgun@gmail.com>
2025-12-14 21:17:30 +0800

    replace build.gradle with a leaner kotlin variant

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("idea")
+    id("java")
+    id("java-library")
+    id("net.neoforged.moddev") version("2.0.78")
+}
+
+version = project.properties["mod_version"]!!
+group = project.properties["mod_group"]!!
+
+repositories {
+    mavenLocal()
+
+    maven("https://maven.neoforged.net/#/releases/")
+    maven("https://www.cursemaven.com")
+    maven("https://api.modrinth.com/maven")
+    maven("https://modmaven.dev")
+    maven("https://maven.createmod.net") // Create, Ponder, Flywheel
+    maven("https://maven.ithundxr.dev/snapshots") // Registrate
+    maven("https://maven.blamejared.com") // JEI, Vazkii's Mods
+}
+
+dependencies {
+    implementation("com.simibubi.create:create-${property("minecraft_version")}:${property("create_version")}:slim") { isTransitive = false }
+    implementation("net.createmod.ponder:Ponder-NeoForge-${property("minecraft_version")}:${property("ponder_version")}")
+    compileOnly("dev.engine-room.flywheel:flywheel-neoforge-api-${property("minecraft_version")}:${property("flywheel_version")}")
+    runtimeOnly("dev.engine-room.flywheel:flywheel-neoforge-${property("minecraft_version")}:${property("flywheel_version")}")
+    implementation("com.tterrag.registrate:Registrate:${property("registrate_version")}")
+}
+
+neoForge {
+    version = property("neo_version").toString()
+
+    parchment {
+        mappingsVersion = property("parchment_mappings_version")!!.toString()
+        minecraftVersion = property("parchment_minecraft_version")!!.toString()
+    }
+
+    runs {
+        configureEach {
+            systemProperty("forge.logging.markers", "REGISTRIES")
+            logLevel.set(org.slf4j.event.Level.DEBUG)
+        }
+
+        create("client") {
+            client()
+            systemProperty("neoforge.enabledGameTestNamespaces", property("mod_id")!!.toString())
+        }
+
+        create("server") {
+            server()
+            programArgument("--nogui")
+            systemProperty("neoforge.enabledGameTestNamespaces", property("mod_id")!!.toString())
+        }
+
+        create("gameTestServer") {
+            type.set("gameTestServer")
+            systemProperty("neoforge.enabledGameTestNamespaces", property("mod_id")!!.toString())
+        }
+    }
+}
+
+tasks.processResources {
+    val props = project.providers.gradlePropertiesPrefixedBy("").get()
+    inputs.properties(props)
+    filesMatching("META-INF/neoforge.mods.toml") { expand(props) }
+}
+
+tasks {
+    jar {
+        archiveBaseName.set("${rootProject.property("mod_id")}-neoforge")
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("src")
+        }
+        resources {
+            srcDir("src/generated/resources")
+        }
+    }
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
