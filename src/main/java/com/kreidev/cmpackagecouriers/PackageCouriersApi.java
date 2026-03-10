026ed7942597938506a89185204382907ca81aee
Krei <criketgun@gmail.com>
2026-03-10 12:27:49 +0800

    add back unpack effect interface as a functional interface use UnpackEffect as consumer

--- src/main/java/com/kreidev/cmpackagecouriers/PackageCouriersApi.java
+++ src/main/java/com/kreidev/cmpackagecouriers/PackageCouriersApi.java
@@ -1,30 +1,31 @@
 package com.kreidev.cmpackagecouriers;
 
+import com.kreidev.cmpackagecouriers.plane.UnpackEffect;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 @SuppressWarnings("unused")
 public class PackageCouriersApi {
 
-    private static final Map<Item, BiConsumer<Level, ItemStack>> ITEM_UNPACK_EFFECTS = new HashMap<>();
+    private static final Map<Item, UnpackEffect> ITEM_UNPACK_EFFECTS = new HashMap<>();
 
-    public static void registerUnpackEffects(Item item, BiConsumer<Level, ItemStack> handler) {
-        ITEM_UNPACK_EFFECTS.put(item, handler);
+    public static void registerUnpackEffects(Item item, UnpackEffect consumer) {
+        ITEM_UNPACK_EFFECTS.put(item, consumer);
     }
 
     public static boolean hasUnpackEffects(Item item) {
         return ITEM_UNPACK_EFFECTS.containsKey(item);
     }
 
-    public static void handleUnpackEffects(Level level, ItemStack stack) {
-        BiConsumer<Level, ItemStack> handler = ITEM_UNPACK_EFFECTS.get(stack.getItem());
-        if (handler != null) {
-            handler.accept(level, stack);
+    public static void handleUnpackEffects(Level level, ItemStack stack, Vec3 planePos, CourierTarget target) {
+        UnpackEffect consumer = ITEM_UNPACK_EFFECTS.get(stack.getItem());
+        if (consumer != null) {
+            consumer.accept(level, stack, planePos, target);
         }
     }
 }
---
+++ src/main/java/com/kreidev/cmpackagecouriers/plane/UnpackEffect.java
@@ -0,0 +1,12 @@
+package com.kreidev.cmpackagecouriers.plane;
+
+import com.kreidev.cmpackagecouriers.CourierTarget;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+@FunctionalInterface
+public interface UnpackEffect {
+
+    void accept(Level level, ItemStack stack, Vec3 planePos, CourierTarget target);
+}
