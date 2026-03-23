2eb3512158a2be08908115f8c6bc2540afdc74dd
Krei <criketgun@gmail.com>
2026-03-23 19:04:02 +0800

    add alt client run config

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,12 +74,19 @@ legacyForge {
             logLevel.set(org.slf4j.event.Level.DEBUG)
         }
 
-        create("client") {
+        create("Client") {
             client()
             systemProperty("forge.enabledGameTestNamespaces", property("mod_id")!!.toString())
         }
 
-        create("server") {
+        create("AltClient") {
+            client()
+            programArguments.addAll("--username", "John_Create")
+            programArguments.addAll("--uuid", "dd71de23-cda9-4509-92e8-bc1e088dd093")
+            systemProperty("neoforge.enabledGameTestNamespaces", property("mod_id")!!.toString())
+        }
+
+        create("Server") {
             server()
             programArgument("--nogui")
             systemProperty("forge.enabledGameTestNamespaces", property("mod_id")!!.toString())
